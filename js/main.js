61ae4d792e6f64593d9324a2c45580a12147e0f3
Vlad <v.zhurabetmen@gmail.com>
2025-02-03 23:56:37 +0200

    work on section ptoject and about (60/100)

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ const overlay = document.querySelector('.overlay');
 const modalBtn = document.getElementById('open-popup')
 const closeBtn = document.querySelector('.welcome__btn-close');
 const modal = document.querySelector('.welcome__popup')
+const moreBtn = document.querySelector('.project__btn');
+const cardsEl = document.querySelector('.project__cards');
 
     burgerBtn.onclick = () => {
             burgerBtn.classList.toggle('active');
@@ -21,4 +23,23 @@ const modal = document.querySelector('.welcome__popup')
         }
     }
 
+     moreBtn.onclick = () => {
+        const newCard = document.createElement('div');
+        newCard.classList.add('project__card');
 
+        newCard.innerHTML = `
+        <div class="project__info">
+                        <h3 class="project__card-title">
+                        Project Name
+                    </h3>
+                        <p class="project__card-subtitle">
+                            I created this personal project in order to show how to create an interface in Figma using a
+                            portfolio as an example.
+                        </p>
+                        <button class="project__card-btn">View Project</button>
+                    </div>
+                    <img class="project__img" src="./public/io.jpeg">
+        `
+
+        cardsEl.appendChild(newCard);
+    }
